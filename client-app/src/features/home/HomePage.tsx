01dd24fbd4179781e5a163869127c02d5facefa3
SAMTEC15 <samuel.orji@decagon.dev>
2024-08-18 13:42:00 +0100

    Grouping Activities by date

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     return (
         <Container style={{marginTop: '7em'}}>
             <h1>Home Page</h1>
-            <h3>Go to <Link to='/activities'>Activities</Link> as</h3>
+            <h3>Go to <Link to='/activities'>Activities</Link> </h3>
         </Container>
     );
 }

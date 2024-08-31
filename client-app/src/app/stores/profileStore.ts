3e9b511bf9f93ab269f4091db793c4a7e9a08cdb
SAMTEC15 <samuel.orji@decagon.dev>
2024-08-31 13:14:09 +0100

    About: Bio display, Challenge

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -58,40 +58,59 @@ export default class ProfileStore {
     setMainPhoto = async (photo: Photo) => {
         this.loading = true;
         try {
-           await agent.Profiles.setMainPhoto(photo.id);
+            await agent.Profiles.setMainPhoto(photo.id);
             store.userStore.setImage(photo.imageUrl);
             runInAction(() => {
-                if(this.profile && this.profile.photos){
+                if (this.profile && this.profile.photos) {
                     this.profile.photos.find(p => p.isMain)!.isMain = false
                     this.profile.photos.find(p => p.id === photo.id)!.isMain = true;
                     this.profile.image = photo.imageUrl;
                     this.loading = false;
                 }
-            })                
-            
+            })
+
         } catch (error) {
             console.log(error);
             runInAction(() => this.loading = false)
-            
+
         }
     }
 
     deletePhoto = async (photo: Photo) => {
         this.loading = true;
         try {
-           await agent.Profiles.deletePhoto(photo.id);            
+            await agent.Profiles.deletePhoto(photo.id);
             runInAction(() => {
-                if(this.profile) {
-                    this.profile.photos = this.profile.photos?.filter(p => p.id !== photo.id);       
+                if (this.profile) {
+                    this.profile.photos = this.profile.photos?.filter(p => p.id !== photo.id);
                     this.loading = false;
                 }
-            })                
-            
+            })
+
         } catch (error) {
             console.log(error);
             runInAction(() => this.loading = false)
-            
+
         }
     }
-    
+
+
+    updateProfile = async (profile: Partial<Profile>) => {
+        this.loading = true;
+        try {
+            await agent.Profiles.updateProfile(profile);
+            runInAction(() => {
+                if (profile.displayName && profile.displayName !==
+                    store.userStore.user?.displayName) {
+                    store.userStore.setDisplayName(profile.displayName);
+                }
+                this.profile = { ...this.profile, ...profile as Profile };
+                this.loading = false;
+            })
+        } catch (error) {
+            console.log(error);
+            runInAction(() => this.loading = false);
+        }
+    }
+
 }
